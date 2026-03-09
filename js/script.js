270cf7fc0e5ef3d2fd1878a3493c30713703023d
Anisha2622 <anishapawar0926@gmail.com>
2026-03-09 21:16:11 +0530

    final change

--- js/script.js
+++ js/script.js
@@ -8,6 +8,20 @@ let message=document.getElementById("message").value
 
 let error=document.getElementById("error")
 
+let slides=document.querySelectorAll(".slide")
+let index=0
+
+function changeSlide(){
+
+slides[index].classList.remove("active")
+
+index=(index+1)%slides.length
+
+slides[index].classList.add("active")
+
+}
+
+setInterval(changeSlide,3000)
 if(name=="" || email=="" || message==""){
 error.textContent="Please fill all fields"
 error.style.color="red"
